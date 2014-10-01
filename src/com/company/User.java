2bdb9babf6b5f7970d3e41f45cf46d2bc4125724
Dima Osadchy <dima.osadchy@gmail.com>
2014-10-01 18:51:04 +0300

    impliment User

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -5,4 +5,36 @@ package com.company;
  */
 public class User {
 
+    protected String name;
+    protected String password;
+    protected BankAccount account = new BankAccount(name, password);
+
+}
+
+class BankAccount{
+    BankAccount(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    protected String name;
+    protected String password;
+
+
 }
