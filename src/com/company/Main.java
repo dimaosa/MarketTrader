389c5ad4cd66e7b330c756bb0fc80d4ec001ff0c
Dima Osadchy <dima.osadchy@gmail.com>
2014-10-01 18:27:48 +0300

    just add a Test line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello all let's start this project");
+        System.out.println("Just add a line");
     }
 }
